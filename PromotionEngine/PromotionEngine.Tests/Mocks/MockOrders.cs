9933e91e8b3c5892dbf4278416fd80fa3f36424d
Karan Negi <NegiKara@ES.AD.ADP.com>
2021-01-21 16:59:29 +0530

    Added Promotion Manager Tests

---
+++ PromotionEngine/PromotionEngine.Tests/Mocks/MockOrders.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.Business.Tests.Mocks
+{
+    public class MockOrders
+    {
+        Products _products = new Products();
+        public List<CartItem> Order1()
+        {
+            return new List<CartItem>()
+            {
+                new CartItem(_products.GetProduct('A'),1),
+                new CartItem(_products.GetProduct('B'),1),
+                new CartItem(_products.GetProduct('C'),1),
+            };
+        }
+        public List<CartItem> Order2()
+        {
+            return new List<CartItem>()
+            {
+                new CartItem(_products.GetProduct('A'),5),
+                new CartItem(_products.GetProduct('B'),5),
+                new CartItem(_products.GetProduct('C'),1),
+            };
+        }
+        public List<CartItem> Order3()
+        {
+            return new List<CartItem>()
+            {
+                new CartItem(_products.GetProduct('A'),3),
+                new CartItem(_products.GetProduct('B'),5),
+                new CartItem(_products.GetProduct('C'),1),
+                new CartItem(_products.GetProduct('D'),1)
+            };
+        }
+    }
+}
---
+++ PromotionEngine/PromotionEngine.Tests/PromotionManagerTests.cs
@@ -0,0 +1,35 @@
+﻿using Moq;
+using PromotionEngine.Business.Tests.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PromotionEngine.Business.Tests
+
+{
+    [Trait("Category", "Business")]
+    public class PromotionManagerTests
+    {
+        private PromotionManager GenerateSUT()
+        {
+            return new PromotionManager();
+        }
+
+        [Fact(DisplayName = "GetPromotionAmount: Should return amount after applying promotions")]
+        public void GetPromotionAmount_WhenItemsArePassed_ShouldApplyAllPromotionAndReturnAmount()
+        {
+            //Arrange            
+            var sut = GenerateSUT();
+            var orders = new MockOrders();
+            var expectedResult = 140;
+
+            //Act
+            var amount = sut.GetPromotionAmount(orders.Order2());
+
+            //Assert                       
+            Assert.Equal(expectedResult, amount);
+        }
+
+    }
+}
